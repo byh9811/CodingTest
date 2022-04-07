0e33665a0077480498977ad440ae0daefeaaf7c3
byh9811 <byh9811@naver.com>
2022-04-07 15:15:17 +0900

    그리디.조이스틱 풀이 업로드

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import 그래프.가장_먼_노드.Solution;
+import 그리디.조이스틱.Solution;
 
 public class Main {
     public static void main(String[] args) {
         int[][] vertex = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
 
-        System.out.println(new Solution().solution(6, vertex));
+        System.out.println(new Solution().solution("JAZ"));
     }
 
     private static void print(int[][] arr) {
