eebf0dda5eb94b7a9390e7696881dc2ca0d8cd65
byh9811 <byh9811@naver.com>
2022-04-19 18:24:12 +0900

    프린터 풀이 수정

--- 프로그래머스/고득점_Kit/스택_및_큐/프린터/Solution.java
+++ 프로그래머스/고득점_Kit/스택_및_큐/프린터/Solution.java
@@ -1,22 +1,21 @@
 package 프로그래머스.고득점_Kit.스택_및_큐.프린터;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    public int solution(int[] priorites, int location) {
+    public int solution(int[] priorities, int location) {
         Queue<Integer> q = new LinkedList<>();
         int answer = 0;
-        for(int priority: priorites)
+        for(int priority: priorities)
             q.add(priority);
-        Arrays.sort(priorites);
-        int idx = priorites.length-1;
+        Arrays.sort(priorities);
+        int idx = priorities.length-1;
 
         while(true) {
             int priority = q.poll();
-            if(priority==priorites[idx]) {
+            if(priority==priorities[idx]) {
                 if(location==0) {
                     answer++;
                     break;
