d4d980c0813e92aa08d669ccb21b2d2ffcdbec9e
byh9811 <byh9811@naver.com>
2022-04-22 12:16:04 +0900

    카카오프렌즈 컬러링북 풀이 업로드

--- 프로그래머스/레벨2/카카오프렌즈_컬러링북/Solution.java
+++ 프로그래머스/레벨2/카카오프렌즈_컬러링북/Solution.java
@@ -1,30 +1,69 @@
 package 프로그래머스.레벨2.카카오프렌즈_컬러링북;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
-    public int solution(int[][] board, int[] moves) {
-        Stack<Integer> bucket = new Stack<>();
-        int answer = 0;
-        for(int move: moves) {
-            int i = 0;
-            for(; i<board.length; i++) {
-                if(board[i][move-1]!=0)
-                    break;
-            }
-            if(i==board.length)
-                continue;
+    public int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+        boolean[][] visited = new boolean[m][n];
 
-            int pick = board[i][move-1];
-            board[i][move-1] = 0;
-            if(bucket.isEmpty() || bucket.peek()!=pick)
-                bucket.add(pick);
-            else {
-                bucket.pop();
-                answer += 2;
+        for(int i=0; i<visited.length; i++) {
+            for(int j=0; j<visited[i].length; j++) {
+                if(!visited[i][j] && picture[i][j]!=0) {
+                    numberOfArea++;
+                    int size = bfs(visited, picture, i, j);
+                    maxSizeOfOneArea = Math.max(size, maxSizeOfOneArea);
+                }
             }
         }
 
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
         return answer;
+    }
+
+    public int bfs(boolean[][] visited, int[][] picture, int i, int j) {
+        Queue<Integer> rowQ = new LinkedList<>();
+        Queue<Integer> colQ = new LinkedList<>();
+        int color = picture[i][j];
+        int size = 0;
+        rowQ.add(i);
+        colQ.add(j);
+        visited[i][j] = true;
+
+        while(!rowQ.isEmpty()) {
+            int row = rowQ.poll();
+            int col = colQ.poll();
+            size++;
+
+            if(row!=0 && !visited[row-1][col] && picture[row-1][col]==color) {
+                rowQ.add(row-1);
+                colQ.add(col);
+                visited[row-1][col] = true;
+            }
+
+            if(row!=visited.length-1 && !visited[row+1][col] && picture[row+1][col]==color) {
+                rowQ.add(row+1);
+                colQ.add(col);
+                visited[row+1][col] = true;
+            }
+
+            if(col!=0 && !visited[row][col-1] && picture[row][col-1]==color) {
+                rowQ.add(row);
+                colQ.add(col-1);
+                visited[row][col-1] = true;
+            }
+
+            if(col!=visited[row].length-1 && !visited[row][col+1] && picture[row][col+1]==color) {
+                rowQ.add(row);
+                colQ.add(col+1);
+                visited[row][col+1] = true;
+            }
+        }
+
+        return size;
     }
 }
