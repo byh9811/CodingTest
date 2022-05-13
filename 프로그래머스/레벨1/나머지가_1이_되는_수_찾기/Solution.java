851367128909f8b145668a2b738d18c811e17dce
byh9811 <byh9811@naver.com>
2022-05-13 14:47:01 +0900

    풀이 수정

--- 프로그래머스/레벨1/나머지가_1이_되는_수_찾기/Solution.java
+++ 프로그래머스/레벨1/나머지가_1이_되는_수_찾기/Solution.java
@@ -1,12 +1,11 @@
 package 프로그래머스.레벨1.나머지가_1이_되는_수_찾기;
 
 public class Solution {
-    public int solution(int[] a, int[] b) {
-        int answer = 0;
-
-        for(int i=0; i<a.length; i++)
-            answer += a[i] * b[i];
-
-        return answer;
+    public int solution(int n) {
+        for(int i=2; i<1000000; i++) {
+            if(n%i==1)
+                return i;
+        }
+        return -1;
     }
 }
