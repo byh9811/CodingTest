e4dc53429b6031ca3c06e2c4c9960fe88d760cc8
byh9811 <byh9811@naver.com>
2022-06-15 16:26:46 +0900

    2개 이하로 다른 비트 풀이 업로드

--- 프로그래머스/레벨2/두개_이하로_다른_비트/Solution.java
+++ 프로그래머스/레벨2/두개_이하로_다른_비트/Solution.java
@@ -2,7 +2,32 @@ package 프로그래머스.레벨2.두개_이하로_다른_비트;
 
 public class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = {};
+        long[] answer = new long[numbers.length];
+
+        for(int i=0; i<numbers.length; i++) {
+            long number = numbers[i];
+
+            if((number&1)==0)
+                answer[i] = number+1;
+            else {
+                int idx = getLastZeroIndex(number);
+                number += Math.pow(2, idx);
+                number -= Math.pow(2, idx-1);
+                answer[i] = number;
+            }
+        }
+
         return answer;
+    }
+
+    private int getLastZeroIndex(long number) {
+        for(int i=0; i<65; i++) {
+            int remain = (int)number % 2;
+            number /= 2;
+            if(remain==0)
+                return i;
+        }
+
+        return -1;
     }
 }
