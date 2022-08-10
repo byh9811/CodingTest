36d77c8532236b61480bde155b16b101bdbe4b38
byh9811 <byh9811@naver.com>
2022-08-10 15:52:28 +0900

    추석 트래픽 풀이 업로드

--- 프로그래머스/레벨3/추석_트래픽/Solution.java
+++ 프로그래머스/레벨3/추석_트래픽/Solution.java
@@ -2,7 +2,38 @@ package 프로그래머스.레벨3.추석_트래픽;
 
 public class Solution {
     public int solution(String[] lines) {
-        int answer = 0;
-        return answer;
+        int max = 0;
+        int[] startTimes = new int[lines.length];
+        int[] endTimes = new int[lines.length];
+
+        parseLines(lines, startTimes, endTimes);
+
+        for(int i=0; i<lines.length; i++) {
+            int task = 1;
+
+            for(int j=i+1; j<lines.length; j++) {
+                if(startTimes[j]<endTimes[i]+1000)
+                    task++;
+            }
+
+            max = Math.max(max, task);
+        }
+
+        return max;
+    }
+
+    private void parseLines(String[] lines, int[] startTimes, int[] endTimes) {
+        for(int i=0; i<lines.length; i++) {
+            int endTime = 0;
+            String[] tokens = lines[i].split(" ");
+            String[] timeInfo = tokens[1].split(":");
+
+            endTime += Integer.parseInt(timeInfo[0]) * 60 * 60 * 1000;
+            endTime += Integer.parseInt(timeInfo[1]) * 60 * 1000;
+            endTime += (int)(Double.parseDouble(timeInfo[2]) * 1000);
+
+            endTimes[i] = endTime;
+            startTimes[i] = endTime - (int)(Double.parseDouble(tokens[2].substring(0, tokens[2].length()-1)) * 1000) + 1;
+        }
     }
 }
