8ced707af3d0e5c653f746c96b1231326b89c334
byh9811 <byh9811@naver.com>
2022-08-19 12:47:32 +0900

    swap 함수 구현

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] int1DArr = {2,1,1,1,0,0,0,0,0,0,0};
+        int[] int1DArr = {1,4,2,3,5,6,9,8,7};
         int[] int1DArr2 = {3, 4};
         int[][] int2DArr = {
                 {1,2,3},
