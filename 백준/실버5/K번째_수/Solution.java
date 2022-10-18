5b5375593b1034e713e68da53580c5e7f735fa76
byh9811 <byh9811@naver.com>
2022-10-18 15:29:59 +0900

    K번째 수 풀이 업로드

--- 백준/실버5/K번째_수/Solution.java
+++ 백준/실버5/K번째_수/Solution.java
@@ -2,10 +2,16 @@ package 백준.실버5.K번째_수;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Solution {
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine().split(" ")[1]);
 
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        System.out.print(arr[K-1]);
     }
 }
