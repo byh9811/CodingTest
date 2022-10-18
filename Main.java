ced97eafd72884270d70669d97368b94bf69d5cc
byh9811 <byh9811@naver.com>
2022-10-18 12:31:13 +0900

    K번째 수 풀이 업로드

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import 백준.실버5.카드1.Solution;
+import 백준.실버5.K번째_수.Solution;
 
 import java.util.Arrays;
 
---
+++ 백준/실버5/K번째_수/Solution.java
@@ -0,0 +1,11 @@
+package 백준.실버5.K번째_수;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
