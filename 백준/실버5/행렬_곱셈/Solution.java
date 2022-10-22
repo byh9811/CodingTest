60bbcd35bea33cd5c1d37cda822aba75b4fd855f
byh9811 <byh9811@naver.com>
2022-10-22 17:06:01 +0900

    행렬 곱셈 풀이 업로드

---
+++ 백준/실버5/행렬_곱셈/Solution.java
@@ -0,0 +1,11 @@
+package 백준.실버5.행렬_곱셈;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
