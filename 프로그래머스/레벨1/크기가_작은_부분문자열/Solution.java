4e0229c0564c8730756d223e08006acbee8ac274
byh9811 <byh9811@naver.com>
2022-12-23 16:33:48 +0900

    크기가 작은 부분 문자열 풀이 업로드

---
+++ 프로그래머스/레벨1/크기가_작은_부분문자열/Solution.java
@@ -0,0 +1,16 @@
+package 프로그래머스.레벨1.크기가_작은_부분문자열;
+
+public class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+
+        for(int i=0; i<t.length()-p.length()+1; i++) {
+            if(Long.parseLong(t.substring(i, i+p.length()))>Long.parseLong(p))
+                continue;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
