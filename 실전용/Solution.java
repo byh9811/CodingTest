766b4e4d178e7e1ccea4b1568b9934fcda0d983a
byh9811 <byh9811@naver.com>
2023-02-11 15:29:00 +0900

    Update Solution.java

--- 실전용/Solution.java
+++ 실전용/Solution.java
@@ -1,10 +1,14 @@
 package 실전용;
 
-import java.net.*;
-import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Solution {
-    public String getPhoneNumbers(String country, String phoneNumber) {
-        return "hello";
-    }
+/**
+ * 1. .을 기준으로 밖으로 나갈 수가 없으면? 도형의 일부
+ * 2. 전체 원소 개수 - 밖으로 나가지는 .의 개수 = 정답
+ * 3. .을 기준으로 BFS ㄱㄱ
+ *
+ */
+class Solution {
+    
 }
